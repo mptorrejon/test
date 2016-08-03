5faf3d69f8a55c6a963c915bfd2cff1a58a519a1
mptorrejon <mptorrejon@gmail.com>
2016-08-02 21:16:21 -0400

    few changes

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
 import { APP_ROUTER_PROVIDERS } from './components/app.routes';
 import { AppComponent } from './components/app.component';
-import { FormlyProviders } from 'ng2-formly';
+import { FormlyProviders } from 'ng2-formly'; 
+import { disableDeprecatedForms, provideForms } from '@angular/forms';
 
 bootstrap( AppComponent, [
 	APP_ROUTER_PROVIDERS,
